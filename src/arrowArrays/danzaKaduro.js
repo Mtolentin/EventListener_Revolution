909269ec2f90cfc3b541a748504cfe9212caacb4
Mtolentin <mattca701@gmail.com>
2020-10-09 14:53:18 -0700

    Readme

--- src/arrowArrays/danzaKaduro.js
+++ src/arrowArrays/danzaKaduro.js
@@ -1,14 +1,12 @@
 let danzaKaduro = [];
 
 danzaKaduro = danzaKaduro.concat(
-    [[12345, "ArrowRight"], [13345, "ArrowRight"], [14345, "ArrowRight"]]
+    [[12345, "Ready"], [13345, "Go"], [14345, "Clear"]]
 );
 
 danzaKaduro = danzaKaduro.concat(
     [[12345, "ArrowRight"], [13345, "ArrowRight"], [14345, "ArrowRight"],
     [12345, "ArrowRight"], [13345, "ArrowRight"], [14345, "ArrowRight"]]
 );
-
-
 
 export default danzaKaduro;
